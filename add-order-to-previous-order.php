198cd407fd77b47430ffd5ea51f4c32f66542886
datahari <info@datahari.fi>
2020-09-07 17:44:22 +0300

    added some comments

--- add-order-to-previous-order.php
+++ add-order-to-previous-order.php
@@ -4,12 +4,14 @@ add_action( 'woocommerce_thankyou', 'dh_combine_orders', 10, 1 );
 add_action( 'woocommerce_checkout_update_order_meta', 'dh_update_order_meta' );
 add_action( 'woocommerce_before_order_notes', 'dh_add_checkout_old_order_field' );
 
+// save number of desired old order to new post metadata
 function dh_update_order_meta( $order_id )  {
 	if ( isset( $_POST['old-order-number'] ) ) {
 		update_post_meta( $order_id, '_dh_old_order_number', sanitize_text_field( $_POST['old-order-number'] ) );
 	}
 }
 
+// add field for previous order number to checkout form
 function dh_add_checkout_old_order_field( $checkout ) { 
 	woocommerce_form_field( 'old-order-number', array(        
 		'type' => 'text',        
@@ -22,6 +24,7 @@ function dh_add_checkout_old_order_field( $checkout ) {
 	), $checkout->get_value( 'old-order-number' ) );
 }
 
+// check if the order number the user gave is valid
 function dh_validate_old_order_field() {    
 	$order_number = trim( rtrim( $_POST['old-order-number'] ) );
 	
@@ -34,16 +37,19 @@ function dh_validate_old_order_field() {
 	}
 }
 
+// check if an order can be added to another one
 function dh_can_add_to_order( $old_order_id, $new_order_id = null ) {
 	$old_order = wc_get_order( $old_order_id );
 	if( ! $old_order ) {
 		return false;
 	}
 	
+	// if old order is not in processing status
 	if( "processing" !== $old_order->get_status() ) {
 		return false;
 	}
 	
+	// if we are only checking status of old order
 	if( null === $new_order_id ) {
 		return true;
 	}
@@ -53,6 +59,7 @@ function dh_can_add_to_order( $old_order_id, $new_order_id = null ) {
 		return false;
 	}
 	
+	// check if billing information matches
 	$old_order_billing = serialize( $old_order->get_address( 'billing' ) );
 	$new_order_billing = serialize( $new_order->get_address( 'billing' ) );
 	
@@ -60,6 +67,7 @@ function dh_can_add_to_order( $old_order_id, $new_order_id = null ) {
 		return false;
 	}
 	
+	// check if shipping information matches
 	$old_order_shipping = serialize( $old_order->get_address( 'shipping' ) );
 	$new_order_shipping = serialize( $new_order->get_address( 'shipping' ) );
 	
@@ -67,6 +75,7 @@ function dh_can_add_to_order( $old_order_id, $new_order_id = null ) {
 		return false;
 	}
 	
+	// if everything is ok, we can combine these orders
 	return true;
 }
 
